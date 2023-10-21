46095cab173203147191a0401d5943c16e96147f
Obada-Yahya <obadayahya.an@gmail.com>
2023-10-21 21:20:48 +0300

    added Domain Models : Employee, MenuItem, Order, OrderItem

---
+++ RestaurantReservationDomain/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservationDomain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int RestaurantId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string position { get; set; }
+    }
+}
---
+++ RestaurantReservationDomain/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservationDomain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ReservationId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public float TotalAmount { get; set; }
+    }
+}
