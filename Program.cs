1a40e9353a5630914b707c2f372e6ae29053b312
Obada-Yahya <obadayahya.an@gmail.com>
2023-10-21 10:56:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
