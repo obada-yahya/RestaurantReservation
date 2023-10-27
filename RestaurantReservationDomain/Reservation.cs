5fba6f12c03e0f36368a5cebbca10514a9aed76d
Obada-Yahya <obadayahya.an@gmail.com>
2023-10-27 09:50:28 +0300

    add missing Reservation model

--- RestaurantReservationDomain/Reservation.cs
+++ RestaurantReservationDomain/Reservation.cs
@@ -1,7 +1,12 @@
 ﻿namespace RestaurantReservationDomain
 {
-    public class Class1
+    public class Reservation
     {
-
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int RestaurantId { get; set;}
+        public int TableId { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public int PartySize { get; set; }
     }
 }
