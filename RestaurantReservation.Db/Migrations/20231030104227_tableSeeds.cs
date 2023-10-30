c57e5c15c8e106e47c0e0f8cc5d4d35336b79b72
Obada-Yahya <obadayahya.an@gmail.com>
2023-10-30 19:17:44 +0200

    fixed the table seed migration

--- RestaurantReservation.Db/Migrations/20231030104227_tableSeeds.cs
+++ RestaurantReservation.Db/Migrations/20231030104227_tableSeeds.cs
@@ -8,21 +8,6 @@ namespace RestaurantReservation.Db.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                table: "Restaurants",
-                keyColumn: "Id",
-                keyValue: 1);
-
-            migrationBuilder.DeleteData(
-                table: "Restaurants",
-                keyColumn: "Id",
-                keyValue: 2);
-
-            migrationBuilder.DeleteData(
-                table: "Restaurants",
-                keyColumn: "Id",
-                keyValue: 3);
-
             migrationBuilder.InsertData(
                 table: "Tables",
                 columns: new[] { "Id", "Capacity", "RestaurantId" },
@@ -49,76 +34,61 @@ namespace RestaurantReservation.Db.Migrations
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 1);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 2);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 3);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 4);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 5);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 6);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 7);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 8);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 9);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 10);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 11);
-
+            
             migrationBuilder.DeleteData(
                 table: "Tables",
                 keyColumn: "Id",
                 keyValue: 12);
-
-            migrationBuilder.InsertData(
-                table: "Restaurants",
-                columns: new[] { "Id", "Address", "Name", "OpeningHours", "PhoneNumber" },
-                values: new object[] { 1, "california", "The Rustic Bistro", "11:00 AM - 9:00 PM", "(555) 123-4567" });
-
-            migrationBuilder.InsertData(
-                table: "Restaurants",
-                columns: new[] { "Id", "Address", "Name", "OpeningHours", "PhoneNumber" },
-                values: new object[] { 2, "New York", "Bella Vita Trattoria", "11:00 AM - 9:00 PM", "(555) 987-6543" });
-
-            migrationBuilder.InsertData(
-                table: "Restaurants",
-                columns: new[] { "Id", "Address", "Name", "OpeningHours", "PhoneNumber" },
-                values: new object[] { 3, "texas", "The Spice Garden Grill", "11:00 AM - 9:00 PM", "(555) 789-0123" });
         }
     }
 }
