ba33acb252cbdf6f775c3b01ad26001f0c945b2e
Obada-Yahya <obadayahya.an@gmail.com>
2023-10-30 11:27:09 +0200

    added new migration

--- RestaurantReservation.Db/Migrations/20231030092637_updated-scheme-relationships.Designer.cs
+++ RestaurantReservation.Db/Migrations/20231030092637_updated-scheme-relationships.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantReservation.Db;
 
@@ -11,9 +12,10 @@ using RestaurantReservation.Db;
 namespace RestaurantReservation.Db.Migrations
 {
     [DbContext(typeof(RestaurantReservationDbContext))]
-    partial class RestaurantReservationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231030092637_updated-scheme-relationships")]
+    partial class updatedschemerelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
