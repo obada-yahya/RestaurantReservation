3704d5e8164833db0f163d2f446d31107c524795
Obada-Yahya <obadayahya.an@gmail.com>
2023-11-01 19:28:07 +0200

    Table  Service

---
+++ Services/TableService.cs
@@ -0,0 +1,79 @@
+﻿using RestaurantReservation.Db;
+using RestaurantReservation.Db.RestaurantReservationDomain;
+
+namespace RestaurantReservation.Services;
+
+public class TableService
+{
+    private readonly RestaurantReservationDbContext _context;
+
+    public TableService(RestaurantReservationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void AddTable(Table table)
+    {
+        try
+        {
+            _context.Tables.Add(table);
+            _context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public IEnumerable<Table> GetTables()
+    {
+        try
+        {
+            return _context.Tables;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        return new List<Table>();
+    }
+    
+    public Table? FindTable(int id)
+    {
+        try
+        {
+            return _context.Tables.Single(table => table.Id == id);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        return null;
+    }
+    
+    public void UpdateTable(Table table)
+    {
+        try
+        {
+            _context.Tables.Update(table);
+            _context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    
+    public void DeleteTable(int id)
+    {
+        try
+        {
+            _context.Tables.Remove(new Table(){Id = id});
+            _context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
