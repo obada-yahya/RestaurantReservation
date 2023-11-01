3a169be935bf3c5a6c7b89c439b063d4aa9d70e6
Obada-Yahya <obadayahya.an@gmail.com>
2023-11-01 21:28:00 +0200

    MenuItem Service

---
+++ Services/MenuItemService.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db;
+using RestaurantReservation.Db.RestaurantReservationDomain;
+
+namespace RestaurantReservation.Services;
+
+public class MenuItemService
+{
+    private readonly RestaurantReservationDbContext _context;
+
+    public MenuItemService(RestaurantReservationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void AddMenuItem(MenuItem menuItem)
+    {
+        try
+        {
+            _context.MenuItems.Add(menuItem);
+            _context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public IEnumerable<MenuItem> GetMenuItems()
+    {
+        try
+        {
+            return _context.MenuItems;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        return new List<MenuItem>();
+    }
+    
+    public MenuItem? FindMenuItem(int id)
+    {
+        try
+        {
+            return _context.MenuItems
+                   .Single(order => order.Id == id);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        return null;
+    }
+    
+    public void UpdateMenuItem(MenuItem menuItem)
+    {
+        try
+        {
+            _context.MenuItems.Update(menuItem);
+            _context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    
+    public void DeleteMenuItem(int id)
+    {
+        try
+        {
+            _context.MenuItems.Remove(new MenuItem(){Id = id});
+            _context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
