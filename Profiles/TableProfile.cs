eabd3923baddab0b86d6db5a2efcc4c8356a110b
Obada-Yahya <obadayahya.an@gmail.com>
2023-12-13 21:41:37 +0200

    added table dtos

---
+++ Profiles/TableProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using RestaurantReservation.Db.RestaurantReservationDomain;
+using RestaurantReservation.Dtos.RestaurantDtos;
+using RestaurantReservation.Dtos.TableDtos;
+
+namespace RestaurantReservation.Profiles;
+
+public class TableProfile : Profile
+{
+    public TableProfile()
+    {
+        CreateMap<TableForCreationDto, Table>();
+        CreateMap<Table, TableDto>();
+        CreateMap<TableDto, Table>();
+        CreateMap<TableForCreationDto, TableDto>();
+        CreateMap<RestaurantForUpdateDto, TableDto>();
+    }
+}
