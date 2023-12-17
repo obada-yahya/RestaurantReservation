3f3d4c8c77ea2ab958a9c407ce66c6bccccacd8a
Obada-Yahya <obadayahya.an@gmail.com>
2023-12-17 11:18:19 +0200

    added order repository

---
+++ Repositories/OrderRepository/IOrdersRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantReservation.Db.RestaurantReservationDomain;
+
+namespace RestaurantReservation.Repositories.OrderRepository;
+
+public interface IOrdersRepository
+{
+    public Task<Order?> AddOrderAsync(Order order);
+    public Task<IEnumerable<Order>> GetOrdersAsync();
+    public Task<Order?> FindOrderAsync(int id);
+    public Task UpdateOrderAsync(Order order);
+    public Task DeleteOrderAsync(int id);
+}
