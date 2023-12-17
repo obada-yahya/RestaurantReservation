022ec5ab4a603a99ffd8bbbc2d414625b0c2eb5d
Obada-Yahya <obadayahya.an@gmail.com>
2023-12-17 12:38:28 +0200

    added Dtos for MenuItem

---
+++ Dtos/MenuItemDtos/MenuItemForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation.Dtos.MenuItemDtos;
+
+public class MenuItemForUpdateDto
+{
+    public int Id { get; set; }
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public float Price {  get; set; }
+}
